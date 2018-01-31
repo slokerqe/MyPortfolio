b172c406b089c07b47fcc784f223e5743157b98b
Sasha <slokerqe@gmail.com>
2018-01-31 15:32:18 +0200

    scrollTopButton

--- js/myjs.js
+++ js/myjs.js
@@ -9,15 +9,7 @@ function scrollFunction() {
   }
 }
 
-
-
 */
-
-
-  
-
-
-
 
 // SCROLL DOWN BUTTON
 //.............../
@@ -125,3 +117,36 @@ var colors = new Array(
   }
   
   setInterval(updateGradient,10);
+
+
+
+
+
+
+
+  // UP BUTTON back to top
+
+  $(document).ready(function(){
+	
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+        $('.scrollToTop').fadeIn();
+      } else {
+        $('.scrollToTop').fadeOut();
+      }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+    
+  });
+
+
+
+
+
+  
